d339cdd4840f5e319136771d56090e30a01274e3
Cristian Vargas <cvg97@misena.edu.co>
2021-03-06 21:03:26 -0500

    Working in register page

---
+++ src/pages/register.js
@@ -0,0 +1,127 @@
+import React from "react";
+import { Form, Input, Button, Typography, Card, Col, Row, message } from "antd";
+import { useHistory } from "react-router-dom";
+import { post } from "../http/http";
+
+import "../styles/login.css";
+
+const { Title } = Typography;
+
+const Register = () => {
+  const history = useHistory();
+  const onFinish = async (user) => {
+    console.log("Success:", user);
+    try {
+      const userCreated = await post("/v1/users", user);
+      if (userCreated.created) {
+        message.success("Te has registrado correctamente");
+        history.push("/dash");
+      } else {
+        message.success("El usuario con el que intentas registrarte ya existe");
+      }
+    } catch (error) {
+      console.error(error);
+      message.success("Ha ocurrido un error al registrarte");
+    }
+  };
+  return (
+    <Row>
+      <Col span={24}>
+        <div className="SignUp">
+          <Title level={3}>Task Timer App</Title>
+          <Card>
+            <Title level={4}>Registrarse</Title>
+            <Form
+              name="basic"
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+            >
+              <Form.Item
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: "Ingresa tu nombre",
+                  },
+                ]}
+              >
+                <Input placeholder="Nombre completo" />
+              </Form.Item>
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Ingresa tu usuario",
+                  },
+                ]}
+              >
+                <Input placeholder="Usuario" />
+              </Form.Item>
+              <Form.Item
+                name={["user", "email"]}
+                rules={[
+                  {
+                    type: "email",
+                  },
+                ]}
+              >
+                <Input placeholder="Correo Electonico" />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Ingresa tu contraseña",
+                  },
+                ]}
+              >
+                <Input.Password placeholder="Contraseña" />
+              </Form.Item>
+              <Form.Item
+                name="confirm"
+                dependencies={["password"]}
+                rules={[
+                  {
+                    required: true,
+                    message: "Confirma tu contraseña",
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue("password") === value) {
+                        return Promise.resolve();
+                      }
+
+                      return Promise.reject(
+                        new Error("Las contraseñas no coinciden")
+                      );
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password placeholder="Contraseña" />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  size="middle"
+                  className="Login_Button"
+                >
+                  Ingresar
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default Register;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./pages/login";
+import Register from "./pages/register";
 import Dasboard from "./pages/dashboard";
 
 const Routers = () => {
@@ -8,6 +9,7 @@ const Routers = () => {
     <Switch>
       <Route exact path="/" component={Login} />
       <Route path="/dash" component={Dasboard} />
+      <Route path="/signup" component={Register} />
     </Switch>
   );
 };
